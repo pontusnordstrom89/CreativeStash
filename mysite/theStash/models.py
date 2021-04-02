5ce6e30cea8f2eb2936d1e5d9cd97b0498a47e48
cybertilla <bertilla.fabris@gmail.com>
2021-04-02 21:01:09 +0200

    add foreignkeys and interest model

--- mysite/theStash/models.py
+++ mysite/theStash/models.py
@@ -1,12 +1,15 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
-class Profil(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    #join_date = models.DateField()
     #profession = models.CharField(max_length=30)
     twitter = models.TextField(blank=True)
     instagram = models.TextField(blank=True)
@@ -14,12 +17,29 @@ class Profil(models.Model):
     behance = models.TextField(blank=True)
     github = models.TextField(blank=True)
 
+    def __str__(self):
+        return f"{self.user.name}:{self.bio}"
+
 
 class Idea(models.Model):
     title = models.CharField(max_length=30)
     #image = models.ImageField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="creator")
     description = models.TextField(blank=True)
+    #an idea can be turned public but is private by default
+    public = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
 
 class Category(models.Model):
-    c_name = models.CharField(max_length=30)
+    c_name = models.CharField(max_length=30, primary_key=True)
+    idea = models.ManyToManyField(Idea, through='Interest', through_fields=('c_name', 'idea'),
+    )
+
+    def __str__(self):
+        return self.name
+
+class Interest(models.Model):
+    c_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
