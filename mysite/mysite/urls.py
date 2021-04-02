15c1ee790861b9a680619998634c8a2250c7d765
cybertilla <bertilla.fabris@gmail.com>
2021-04-02 21:01:09 +0200

    add foreignkeys and interest model

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,5 +24,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+#might uncomment for image upload
+"""
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""
--- mysite/theStash/models.py
+++ mysite/theStash/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Profil(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Profile(models.Model):
+    #username = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+    #first_name = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+    #last_name = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+    #date_joined = models.ForeignKey(auth_user, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    #join_date = models.DateField()
     #profession = models.CharField(max_length=30)
     twitter = models.TextField(blank=True)
     instagram = models.TextField(blank=True)
@@ -14,12 +15,27 @@ class Profil(models.Model):
     behance = models.TextField(blank=True)
     github = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
 
 class Idea(models.Model):
     title = models.CharField(max_length=30)
     #image = models.ImageField()
+    username = models.ForeignKey(Profile, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
+    #an idea can be turned public but is private by default
+    public = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
-    c_name = models.CharField(max_length=30)
+    c_name = models.CharField(max_length=30, primary_key=True)
+    idea = models.ManyToManyField(Idea, through='Interest')
+
+    def __str__(self):
+        return self.name
+
+class Interest(models.Model):
+    c_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
