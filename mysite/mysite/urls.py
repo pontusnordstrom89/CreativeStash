81b8a40d302369b9e03715849612cfd742b1e2eb
cybertilla <bertilla.fabris@gmail.com>
2021-04-02 12:42:19 +0200

    a first try of models

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('theStash.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/theStash/models.py
+++ mysite/theStash/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Profil(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.TextField(blank=True)
+    #join_date = models.DateField()
+    #profession = models.CharField(max_length=30)
+    twitter = models.TextField(blank=True)
+    instagram = models.TextField(blank=True)
+    vimeo = models.TextField(blank=True)
+    behance = models.TextField(blank=True)
+    github = models.TextField(blank=True)
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=30)
+    #image = models.ImageField()
+    description = models.TextField(blank=True)
+
+
+class Category(models.Model):
+    c_name = models.CharField(max_length=30)
