72e9039e1e2f38d1803d2120c8175f90dd847019
TobiasSvenssonDev <tobias_svensson84@live.se>
2021-04-14 12:14:41 +0200

    kodgranskningsändringar

--- mysite/stashEditor/views.py
+++ mysite/stashEditor/views.py
@@ -8,7 +8,7 @@ from theStash.models import Idea, Category
 from django.contrib.auth.models import User
 
 def index(request):
-    latest_idea_list = Idea.objects.order_by('-idea_title')
+    latest_idea_list = Idea.objects.order_by('idea_title')
     template = loader.get_template('stashEditor/index.html')
     context = {
         'latest_idea_list': latest_idea_list
@@ -31,21 +31,20 @@ def create_category(request):
 def create(request):
     template = loader.get_template('stashEditor/create.html')
 
-    
     create_idea_form = CreateIdeaForm(request.POST or None, request.FILES or None)
 
     category = create_category(request)
-    # create object of form
-    
+       
 
-     # check if form data is valid
+    # check if form data is valid
     if create_idea_form.is_valid():
         # Return an object without saving to the DB
         form_object = create_idea_form.save(commit=False)
         # Add an author field which will contain current user's id
         form_object.creator = User.objects.get(pk=request.user.id)
-        form_object.save()  # Save the final "real form" to the DB
-        # save the form data to model
+        # Save the final "real form" to the DB
+        form_object.save()  
+        
         return redirect('/')
 
     
