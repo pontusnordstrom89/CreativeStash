2a63ece34ef317d6440a22889ea17bd0e78cb2d2
Pontus Nordström <pontusnordstrom89@gmail.com>
2021-05-24 19:17:31 +0200

    bottstrap styling

--- mysite/stashProfile/forms.py
+++ mysite/stashProfile/forms.py
@@ -4,7 +4,6 @@ from theStash.models import Profile, Idea
 class EditSocialProfile(forms.ModelForm):
     class Meta:
         model= Profile
-        fields = ('bio', 'link1', 'link2', 'link3', 'profile_picture')
         exclude = ['user']
 
 
