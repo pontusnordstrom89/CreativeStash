83f0a4e6d00b6e52de7332c0157186a4d820a59e
TereseAhlin <ahlin.terese@gmail.com>
2021-06-01 18:40:45 +0200

    like funktion

--- mysite/staticfiles/js/globalscripts.js
+++ mysite/staticfiles/js/globalscripts.js
@@ -2,17 +2,4 @@ $("#show-add-category").click(function () {
     $("#add-category-form").toggle();
 });
 
-$("#{{idea_data.idea_id}}").click(function () {
-    $.ajax({
-        url: '/theStash/ajax/like_counter/',
-        data: {
-            'idea_id': idea_data.idea_id
-        },
-        dataType: 'json',
-        success: function (data) {
-            $("#{{idea_data.id}}").text(data);
-        }
-    });
-    
-});
 
